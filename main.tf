8e17dba446d60d4eadac50bf93af8164a58bcb0a
lokeshkr31 <73345870+lokeshkr31@users.noreply.github.com>
2024-07-26 04:47:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "lokesh-s3bucket-date"
+	bucket = "loki-s3bucket-date"
 }
 
 resource "aws_s3_bucket" "bucket2" {
@@ -12,10 +12,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "lokesh-s3bucket-date"
+    bucket         = "loki-s3bucket-date"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "lokesh-date-dblocks"
+    dynamodb_table = "loki-date-dblocks"
     encrypt        = true
   }
 }
